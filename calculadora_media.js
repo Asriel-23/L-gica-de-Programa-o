f36c7c46ac6fdb65e14529c8577abba195364c46
MatheusH <matheushenriquecostamonteiro@gmail.com>
2026-03-10 12:03:18 -0300

    funcoes

---
+++ calculadora_media.js
@@ -0,0 +1,8 @@
+ const calcularMedia = function(notas1, notas2, notas3) {
+    return (notas1 + notas2 + notas3) / 3;
+ }
+ const media = calcularMedia(3, 7, 2);
+ if (media <7 )console.log("Reprovado");
+else console.log("Aprovado");
+
+   
