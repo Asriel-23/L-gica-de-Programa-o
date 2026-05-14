07627a28a691be40a053094a73bc24bfbd435989
matheus <matheushenriquecostamonteiro@gmail.com>
2026-05-14 11:59:46 -0300

    getElement

---
+++ Atividade_09/javascript.js
@@ -0,0 +1,19 @@
+const demoId = document.getElementById("demo");                 
+demoId.style.border = '2px solid purple';
+demoId.textcontent = 'Mudei o texto pelo id';
+demoId.style.backgroundColor = "magenta";
+demoId.style.color = "white";
+
+const catClass = document.getElementsByClassName("categoria"); 
+for (i = 0; i < catClass.length; i++) {
+catClass[i].style.border = '2px dotted orange'; 
+}
+const tag = document.getElementsByTagName("article"); 
+for (i = 0; i < tag.length; i++) {
+tag[i].style.border = '2px dotted blue'; 
+}
+const demoQuery = document.querySelectorAll('.demo-query-all')
+demoQuery.forEach(query =>{
+    query.style.border = '1px solid green'
+})
+
